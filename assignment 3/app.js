453c8d081b13f3e550dbe570a603706a40a0229a
Talha Asif <98359013+TalhahAsif@users.noreply.github.com>
2022-04-12 07:11:57 +0500

    Add files via upload

---
+++ assignment 3/app.js
@@ -0,0 +1,155 @@
+////////////////assignment 21 - 25 ////////////////
+
+// 1 que
+
+// var firstname = prompt("Enter first name ")
+// var lastname = prompt("Enter last name ")
+// var fullname = firstname +" "+ lastname
+//  
+// document.write("<h3>Hello "+fullname+" </h3>")
+
+
+// 2 que
+
+// var mobile = prompt("Enter your favorite mobile phone model")
+// var lenght = mobile.length
+
+// document.write("<h3>My favorite phone is "+mobile+" </h3>")
+// document.write("<h3>lenght of string is "+lenght+" </h3>")
+
+
+// 3 que 
+
+// var aaa = "pakistani"
+// var letter = "n"
+// 
+// document.write("<h3> String: "+aaa+" </h3>")
+// document.write("<h3>index of "+letter +" is " + aaa.indexOf(letter) +" </h3>")
+
+
+// 4 que 
+
+// var aaa = "hello world"
+// var letter = "l"
+
+// document.write("<h3> String: "+aaa+" </h3>")
+// document.write("<h3>index of "+letter +" is " + aaa.lastIndexOf(letter) +" </h3>")
+
+
+// 5 que 
+
+// var aaa = "pakistani"
+// var letter = aaa.charAt(3)
+// 
+// console.log(letter)
+// document.write("<h3>String: "+aaa+"</h3>")
+// document.write("<h3>Character at index 3: "+ letter+"</h3>")
+
+
+// 6 que
+
+// var firstname = prompt("Enter first name ")
+
+// var lastname = prompt("Enter last name ")
+// var fullname = (firstname.concat(lastname)) 
+
+// document.write("<h3>Hello "+fullname+" </h3>")
+
+
+// 7 que
+
+// var city = "Hyderabad"
+// var replace = city.replace("Hyder","islam")
+
+// document.write("<h3>City: "+city+"</h3>")
+// document.write("<h3>After Replacement: "+replace+"</h3>")
+
+// 8 que 
+
+// var message = "Ali and Sami are best friends. They play cricket and football together."
+// var replace = message.replace(/and/g , "&")
+// 
+// document.write("<h3>Message: "+message+"</h3>")
+// document.write("<h3>After Replacement: "+replace+"</h3>")
+
+// 9 que 
+
+// var string = "472"
+// var num = parseInt(string)
+
+// console.log(num)
+
+
+// 10 que 
+
+// var userinput = prompt("Enter")
+// var result = userinput.toUpperCase()
+
+// document.write("<h3>User Input: "+userinput+"</h3>")
+// document.write("<h3>Upper case: "+result+"</h3>")
+
+
+// 11 que 
+
+// var word = prompt("Enter Word")
+// var firstLetter = word.slice(0,1).toUpperCase()
+// var otherletter = word.slice(1).toLowerCase()
+// var titlecase = firstLetter + otherletter
+// 
+// document.write("<h3>User input: "+word+"</h3>")
+// document.write("<h3>Title case: "+titlecase+"</h3>")
+
+
+// 12 que
+
+// var num = 35.36
+// var string = num.toString()
+
+// document.write("<h3>Number: "+num+"</h3>")
+// document.write("<h3>result: "+string+"</h3>")
+
+
+// 13 que
+
+// -----------------------
+
+
+// 14 que
+
+// var A = ["cake","apple pie","cookie","patties"] 
+// var userinput = prompt("enter here").toLowerCase()
+// var flag = false
+// 
+// for(var i=0; i<= A.length; i++){
+    // if(userinput == A[i]){
+        // document.write("This item is available")
+        // flag = true
+    // }
+// }
+// if(flag == false){
+    // document.write("This item is not available ")
+    // flag = false
+// }
+
+
+// 15 que
+ 
+// ------------------------  
+
+
+// 16 que
+
+// var university = "University of Karachi"
+// 
+// for (var i = 0; i<university.length; i++){
+    // document.write("<h3>"+university[i]+"</h3>")
+// }
+
+
+// 17 que
+
+// var aaa = prompt("Enter")
+// var bbb = aaa.slice(-1)
+
+// document.write("<h3>User Input: "+aaa+"</h3>")
+// document.write("<h3>Last character of input: "+bbb+"</h3>")

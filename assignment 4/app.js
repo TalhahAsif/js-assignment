31868464d50552fbc8c3dfb0f1ed500da6476ef6
Talha Asif <98359013+TalhahAsif@users.noreply.github.com>
2022-04-24 03:12:03 +0500

    Add files via upload

---
+++ assignment 4/app.js
@@ -0,0 +1,69 @@
+///////////assignment no 4///////////////
+
+// 1 que
+
+// var userInput = +prompt("Enter Positive number")
+// var round = Math.round(userInput)
+// var floor = Math.floor(userInput)
+// var ceil = Math.ceil(userInput)
+// 
+// document.write("<h3>number is "+ userInput +"</h3>")
+// document.write("<h3>Round off value: "+ round +"</h3>")
+// document.write("<h3>Floor value "+ floor +"</h3>")
+// document.write("<h3> Ceil Valuev"+ ceil +"</h3>")
+
+// 2 que
+
+// var userInput = +prompt("Enter negative number")
+// var round = Math.round(userInput)
+// var floor = Math.floor(userInput)
+// var ceil = Math.ceil(userInput)
+// 
+// document.write("<h3>number is "+ userInput +"</h3>")
+// document.write("<h3>Round off value: "+ round +"</h3>")
+// document.write("<h3>Floor value "+ floor +"</h3>")
+// document.write("<h3> Ceil Valuev"+ ceil +"</h3>") 
+
+// 3 que
+
+// var num = -4
+// var abs = Math.abs(num)
+// 
+// document.write("<h3>The absolute value of "+ num +" is "+ abs +"</h3>")
+
+// 4 que
+
+// var num = Math.floor(Math.random()*6+1)
+
+// document.write("<h3>Random Dice Value: "+num+" </h3>")
+
+// 5 que
+
+// var num = Math.floor(Math.random()*2+1)
+// 
+// if(num == 1){
+    // document.write("its Tails")
+// }else{
+    // document.write("its Heads")
+// }
+
+// 6 que
+
+// var num = Math.floor(Math.random()*100+1)
+
+// document.write("<h3>Random number between 1 to 100: "+num+" </h3>")
+
+// 7 que
+
+// ----------------------------------
+
+// 8 que
+
+// var secNum = 8
+// var userNum = prompt("Fins the secret Numbar bewtween 1 to 10")
+// 
+// if(secNum == userNum){
+    // document.write("<h3>CONGO! your guess is right</h3>")
+// }else(
+    // document.write("<h3>Hard luck :(</h3>")
+// )
